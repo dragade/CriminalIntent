e1d8d126230ee8c71d12a5b45f7de650dae9d31f
Dhananjay Ragade <dragade@gmail.com>
2014-09-16 16:26:20 -0700

    after adding view pager

--- app/src/main/java/com/bignerdranch/android/criminalintent/CrimeListFragment.java
+++ app/src/main/java/com/bignerdranch/android/criminalintent/CrimeListFragment.java
@@ -4,7 +4,6 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.ListFragment;
 import android.text.format.DateFormat;
-import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
@@ -39,7 +38,7 @@ public class CrimeListFragment extends ListFragment {
 
   public void onListItemClick(ListView l, View v, int position, long id) { // Get the Crime from the adapter
     Crime c = mCrimeAdapter.getItem(position);
-    Intent i = new Intent(getActivity(), CrimeActivity.class);
+    Intent i = new Intent(getActivity(), CrimePagerActivity.class);
     i.putExtra(CrimeFragment.EXTRA_CRIME_ID, c.getId());
     startActivity(i);
   }
