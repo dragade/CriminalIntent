be49aca8b63c0d4c924cde8b0376677199bc9f5d
Dhananjay Ragade <dragade@gmail.com>
2014-09-16 11:08:50 -0700

    added initial fragment layout widgets

--- app/src/main/java/com/bignerdranch/android/criminalintent/CrimeFragment.java
+++ app/src/main/java/com/bignerdranch/android/criminalintent/CrimeFragment.java
@@ -8,6 +8,9 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.EditText;
 
 public class CrimeFragment extends Fragment {
@@ -15,6 +18,8 @@ public class CrimeFragment extends Fragment {
 
   private Crime mCrime;
   private EditText mTitleField;
+  private Button mDateButton;
+  private CheckBox mSolvedCheckBox;
 
   @Override
   public void onCreate(Bundle savedInstanceState) {
@@ -44,6 +49,19 @@ public class CrimeFragment extends Fragment {
         // This one too
       }
     });
+
+    mDateButton = (Button) v.findViewById(R.id.crime_date_button);
+    mDateButton.setText(mCrime.getDate().toString());
+    mDateButton.setEnabled(false);
+
+    mSolvedCheckBox = (CheckBox) v.findViewById(R.id.crime_solved_checkbox);
+    mSolvedCheckBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+      @Override
+      public void onCheckedChanged(CompoundButton compoundButton, boolean isChecked) {
+        mCrime.setSolved(isChecked);
+      }
+    });
+
     return v;
   }
 }
